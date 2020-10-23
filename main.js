e0a2bd55ebd08efa858015f29f709b55ebb33bf8
Sione Tenifa <sionetenifa@Siones-MacBook-Pro.local>
2020-10-23 15:51:07 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector("button")
+
+btn.addEventListener('mouseover', function(e){
+    console.log("check")
+    e.target.style.color = "purple"
+})
+
+document.addEventListener('mousemove', function(e) {
+	
+	const r = Math.round(e.pageX * 255 / window.innerWidth);
+	const b = Math.round(e.pageY * 255 / window.innerHeight);
+	const color = `rgb(${r}, 0,${b})`;
+	document.body.style.backgroundColor = color;
+	console.log(color);
+});
